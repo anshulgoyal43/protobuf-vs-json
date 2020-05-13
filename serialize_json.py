15256ee372b23f291d00b343724d32f19474e9c5
anshulgoyal43 <65175818+anshulgoyal43@users.noreply.github.com>
2020-05-13 09:44:33 +0530

    Update serialize_json.py

--- serialize_json.py
+++ serialize_json.py
@@ -19,14 +19,13 @@ for record in data:
     dict_marks = {}
     course_name = list_record[i].split(",")[0]
     if i == len(list_record):
-      course_marks = int(list_record[i].split(",")[1][:-1])
+      course_marks = int(list_record[i].split(",")[1][:-1]) #why/what ?
     else:
       course_marks = int(list_record[i].split(",")[1])
     dict_marks["CourseName"] = course_name
     dict_marks["CourseScore"] = course_marks
     list_marks.append(dict_marks)
   dict_record["CourseMarks"] = list_marks
-  dict_record["RollNo"] = int(list_record[0].split(",")[1])
   final_list.append(dict_record)
 
 with open(sys.argv[2], 'w') as outfile:
@@ -34,5 +33,6 @@ with open(sys.argv[2], 'w') as outfile:
     json.dump(final_list, outfile, separators=(',', ':'),
         ensure_ascii=False)
     end = time.clock()
-
+#if "TIME" in os.environ: ??
+#  print "Serialization time", end - start
 print "Serialization time", end - start
